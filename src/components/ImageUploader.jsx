145e88db447f76bb3fb249791a6c0ce9c94e5cd6
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-18 13:40:30 +0530

    images adding and removing and drap and drop is successfully working

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactImageUploading from "react-images-uploading";
 
 const ImageUploader = () => {
-  return (
-    <div>ImageUploader</div>
-  )
-}
+  const [images, setImages] = useState([]);
+  const maxNumber = 2;
 
-export default ImageUploader
+  const handleChange = (imageList, addUpdateIndex) => {
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+  return (
+    <div>
+      <ReactImageUploading
+        multiple
+        value={images}
+        onChange={handleChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          <div>
+            <button onClick={onImageUpload} {...dragProps}>
+              Click Or Drag here
+            </button>
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index}>
+                <img src={image["data_url"]} alt="" width="100" />
+                <div>
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ReactImageUploading>
+      <h1>hey there</h1>
+    </div>
+  );
+};
+
+export default ImageUploader;
